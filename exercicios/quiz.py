7c3f34efb87ee48aad5045d4ee1871630505f606
Kamily Matias <kleberisaac900@gmail.com>
2026-03-03 09:17:37 -0300

    dvv

---
+++ exercicios/quiz.py
@@ -0,0 +1,4 @@
+dia = int(input('Digite o dia: '))
+mes = input('Digite o mês: ')
+ano = int(input('Digite o ano: '))
+print('Nossa atividade está sendo realizada no dia {}, do mês {} e do ano {}'.format(dia, mes, ano))
