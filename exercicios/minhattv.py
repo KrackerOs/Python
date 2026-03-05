57d0faca93337bf12b98b5b1bcb70b7b24e40d9f
Kamily Matias <kleberisaac900@gmail.com>
2026-03-05 07:41:49 -0300

    sadas

---
+++ exercicios/minhattv.py
@@ -0,0 +1,7 @@
+real1 = float(input('Digite a quantidade money: '))
+dolar = real1*5.22
+print('Você escolheu a quantidade {} de money, convertendo o dolar em real, então teremos {}'.format(real1, dolar))
+
+seila = int(input('Digite um numero: '))
+raiz = seila**4
+print('O numero digitado é {}, multiplicado pela raiz quadrada, é igual a {}'.format(seila, raiz))
