506105abfe314410789466e9b8521f6f6617e3d8
Kamily Matias <kleberisaac900@gmail.com>
2026-03-11 10:56:27 -0300

    ,hhkjh

---
+++ exercicios/ex10.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite quantos metros: '))
+cm = n1*100
+mm = n1*1000
+print('A quantidade de metros digitado foi {}, convertido em Centímetros da {}, ja convertido em milimetros da {}'.format(n1, cm, mm))
---
+++ exercicios/ex11.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um numero: '))
+n2 = n1*1
+n3 = n1*2
+n4 = n1*3
+n5 = n1*4
+n6 = n1*5
+n7 = n1*6
+n8 = n1*7
+n9 = n1*8
+n10 = n1*9
+n11 = n1*10
+print('A tabuada do numero digitado foi {}, multiplicado por 1 da {}, multiplicado por 2 {}, multiplicado por 3 {}, multiplicado por 4 {}, multiplicado por 5 {}, multiplicado por 6 {}, multiplicado por 7 {}, multiplicado por 8 {}, multiplicado por 9 {}, multiplicado por 10 {}'.format(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11))
