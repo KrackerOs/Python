2ebd668fd8c0d5c73d3d99b5f13ff83a3b8693be
Kamily Matias <kleberisaac900@gmail.com>
2026-03-16 08:17:35 -0300

    asdas

---
+++ exercicios/ex12.py
@@ -0,0 +1,3 @@
+real = float(input('Digite a quantidade de valor em dolar: '))
+dolar = n1*5.38
+print('O valor digitado em dolar foi {}, convertendo o valor em real teremos {}'.format(real, dolar))
---
+++ exercicios/ex13.py
@@ -0,0 +1,5 @@
+largura = int(input('Digite a largura: '))
+altura = int(input('Digite a altura: '))
+area = largura*altura 
+tinta = area/2
+print('A largura digitada em metros foi {}, ja a quantidade de metros de altura digitada foi {}, com isso em mente, a quantidade de area é {}, necessitando de {} de tinta'.format(largura, altura, area, tinta))
