a5106c039e2fc50ddb6c29786335f59da8687a7f
Kamily Matias <kleberisaac900@gmail.com>
2026-03-17 12:10:42 -0300

    lhj

---
+++ exercicios/ex15.py
@@ -0,0 +1,3 @@
+produto = float(input('Digite o valor do produto: '))
+desconto = produto - (produto*5/100)
+print('O valor do produto digitado foi {}, com o desconto sendo de {}'.format(produto, desconto))
