f1598f49226b5db7fb2b9f6d7455d6c1f3cf1189
Kamily Matias <kleberisaac900@gmail.com>
2026-03-17 12:27:49 -0300

    ex16

---
+++ exercicios/ex16.py
@@ -0,0 +1,3 @@
+funcionario = float(input('Qual o salário do funcionario?: '))
+aumento = funcionario + (funcionario*15/100)
+print('O salario normal do funcionario digitado foi {}, com o aumento, o funcionario passa a receber {}'.format(funcionario, aumento))
