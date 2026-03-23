b6ccf43d41afd1b5b27720de152a71f2e717b13f
Kamily Matias <kleberisaac900@gmail.com>
2026-03-23 12:22:06 -0300

    \czxczx

--- exercicios/conteudo/cont04.py
+++ exercicios/conteudo/cont04.py
@@ -1,4 +1,11 @@
-import math 
-num1 = int(input('Digite um Numero: '))
-raiz = math.sqrt(num1)
-print('A raiz de {} é igual a {}'.format(num1, raiz))
+#import math 
+#num1 = int(input('Digite um Numero: '))
+#raiz = math.sqrt(num1)
+#print('A raiz de {} é igual a {}'.format(num1, math.ceil(raiz))) #Ceil arredonda um numero para cima
+#print('A raiz de {} é igual a {}'.format(num1, math.floor(raiz))) #floor arredonda a nota para um numero abaixo
+ 
+#Importando funcionalidades especificas
+
+from math import sqrt, floor 
+num01 = int(input('Digite seu numero: '))
+raiz = sqrt(num01)
