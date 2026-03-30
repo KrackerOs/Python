009f486ea2a7f00ff6a45b3cafb7443c92da329f
Kamily Matias <kleberisaac900@gmail.com>
2026-03-30 08:11:36 -0300

    sadasd

--- exercicios/conteudo/cont05.py
+++ exercicios/conteudo/cont05.py
@@ -1,1 +1,6 @@
 import random
+num = random.random()
+num02 = random.randint(1,10)
+print(num)
+print(num02)
+
---
+++ exercicios/exe18.py
@@ -0,0 +1,3 @@
+from math import trunc
+num01 = float(input('Digite um numero real: '))
+print('O valor digitado foi {}, e sua fração inteira é {}'.format(num01, trunc(num01)))
