b5a3c27f3c3950f11e94840274ee7baabaf9dca7
Kamily Matias <kleberisaac900@gmail.com>
2026-03-31 12:08:11 -0300

    asd

--- exercicios/ex20.py
+++ exercicios/ex20.py
@@ -1,4 +1,2 @@
-from math import acos, asin, atan 
-ang = float(input('Digite um angulo:'))
-
-print('Os valores de cosseno {}, seno {} e tangente {} digitados são iguais a'.format(valores))
+import math
+ang = float(input('Digite o ângulo que você deseja: '))
