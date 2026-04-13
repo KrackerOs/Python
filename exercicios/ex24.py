aa77c50b2603631a9aef5edf81c5e1e7cf9a67b4
Kleber <kleberisaac900@gmail.com>
2026-04-13 07:46:07 -0300

    asdas

---
+++ exercicios/ex24.py
@@ -0,0 +1,6 @@
+nome = str(input('Escreva seu nome: ').strip)
+dividido = nome.split()
+
+print('Analisando seu nome... ')
+print('Seu nome em maiusculas é'.format(nome.upper()))
+
---
+++ exercicios/ex25.py
@@ -0,0 +1,10 @@
+num = int(input('Escreva um numero: '))
+
+uni = num // 1%10
+print(uni)
+d = num // 10%10
+print(d)
+c = num // 100%10
+print(c)
+m = num // 1000%10
+print(m)
