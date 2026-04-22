1bf945ec15c3e96465d5bccd7ccc6ea7b3ba761e
Kleber <kleberisaac900@gmail.com>
2026-04-22 09:51:42 -0300

    asdasd

---
+++ exercicios/ex28.py
@@ -0,0 +1,3 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+print('A quantidade de vezes que o A aparece na primeira frase é {}'.format(frase.count('A')+1))
+print('A quantidade de vezes que o A aparece da ultima')
